297b9a37f4d269dd8aa72b2663d0034c2041912d
Vanessa Dinh <vd2407@cumc.columbia.edu>
2023-04-19 15:26:26 -0400

    -adding additional functions

--- p8477_dengue_final.R
+++ p8477_dengue_final.R
@@ -2,9 +2,9 @@
 
 library(deSolve)
 
-# basic model
+# basic model, no insecticide use
 
-SEIR_vector <- function(t, state, parameters) {
+SEIR_basic_no_fog <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
     
     # host population
@@ -27,9 +27,8 @@ SEIR_vector <- function(t, state, parameters) {
 
 # parameter values for simulations
 
-
 NH = 1e4    # host population
-TLH = 68.5*365*24 # host life span is 68.5 years or 600,060 hours or 25002.5 days
+TLH = 68.5*365 # host life span is 68.5 years or 600,060 hours or 25002.5 days
 TIIT = 5  # intrinsic incubation period is 5 days
 TEIT = 10   # extrinsic incubation period is 10 days
 MPP = 2    # Number of mosquitoes per person, MPP = 2 for simulation 1-3
@@ -41,20 +40,20 @@ cHV = 0.375   # effective contact rate, host to vector is 0.375/day
 
 # average TLV
 
-TLV = 3.5
+TLV = 4
 
 # initial states (not directly from paper)
 
-SH = NH - EH - IH - RH
-EH = 0.5*SH  # initial number of exposed in humans (0.5*SH)
-IH =  0.25*EH # initial number of infections in humans (0.25*EH)
+EH = 0  # initial number of exposed in humans 
+IH = 0  # initial number of infections in humans 
 RH = 0 # ???
+SH = NH - EH - IH - RH
 
-SV = MPP*NH # ratio of mosquitoes to humans
-EV = 0.5*SV # initial number of exposed in mosquitoes (0.5*SV)
-IV = 0.029*EV # initial number of infectious in mosquitoes (0.029*EV)
+SV = 20000 # ratio of mosquitoes to humans
+EV = 0 # initial number of exposed in mosquitoes (0.5*SV)
+IV = 1 # initial number of infectious in mosquitoes 
 
-times = 1:(365) # ???
+times=seq(1,365) # ???
 
 state = c(SH = SH, EH = EH, IH = IH, RH = RH,
           SV = SV, EV = EV, IV = IV)
@@ -63,20 +62,94 @@ param = c(TLH = TLH, TIIT = TIIT, TEIT = TEIT, MPP = MPP, e = e,
           IH_visit = IH_visit, TID = TID, cVH = cVH, cHV = cHV,
           TLV = TLV)
 
-sim_basic = ode(y=state,times=times,func=SEIR_vector,parms=param)
+sim_basic = ode(y=state,times=times,func=SEIR_basic_no_fog,parms=param)
 
-
+#matplot(sim_basic[,'time'], sim_basic[,'IV'], type = 'l', lwd = 1, col = 'blue', lty = 1)
 matplot(sim_basic[,'time'], sim_basic[,'IH'], type = 'l', xlim = c(0,365), lwd = 1, col = 'red',
         lty = 1, main = 'Human Infections of Dengue', cex.main = 1, ylab = 'Prevalence of Dengue', xlab = 'Time')
 legend('topright',cex=1,seg.len = 1,
-       legend='IH',
+       legend=c('IH'),
        lty=c(1,1),lwd=c(1,1),
-       col='red',bty='n')
+       col=c('red'),bty='n')
 
 
-# model function for seasonality
+# basic model with insecticide use
 
-SEIR_vector <- function(t, state, parameters) {
+SEIR_basic_fog <- function(t, state, parameters) {
+  with(as.list(c(state, parameters)), {
+    
+    # beta = beta0 * (1 + b.term * Term[time])
+    
+    # host population
+    dSH = (NH/TLH) - (SH * (cVH * (IV/NH) + (1/TLH)))
+    dEH = (SH * cVH * (IV/NH)) - (EH * ((1/TIIT) + (1/TLH)))
+    dIH = (EH/TIIT) - (IH * ((1/TID) + (1/TLH)))
+    dRH = (IH/TID) - (RH/TLH)
+    
+    # vector population
+    
+    dSV = e - (SV * (cHV * ((IH + IH_visit)/NH) + (1/TLV)))
+    dEV = (SV * (cHV * ((IH + IH_visit)/NH))) - (EV * ((1/TEIT) + (1/TLV)))
+    dIV = (EV/TEIT) - (IV/TLV)
+    
+    list(c(dSH, dEH, dIH, dRH, dSV, dEV, dIV))
+    
+  })
+}
+
+
+# parameter values for simulations
+
+NH = 1e4    # host population
+TLH = 68.5*365 # host life span is 68.5 years or 600,060 hours or 25002.5 days
+TIIT = 5  # intrinsic incubation period is 5 days
+TEIT = 10   # extrinsic incubation period is 10 days
+MPP = 2    # Number of mosquitoes per person, MPP = 2 for simulation 1-3
+e = 5000    # emerging rate of adult mosquitoes, 5000 for simulation 1-3
+IH_visit = 0  # visiting infectious host
+TID = 3   # host infection duration is 3 days, AKA gamma
+cVH = 0.75  # effective contact rate, vector to host is 0.75/day
+cHV = 0.375   # effective contact rate, host to vector is 0.375/day
+
+# average TLV
+
+TLV = 4
+
+# initial states (not directly from paper)
+
+EH = 0  # initial number of exposed in humans 
+IH = 0  # initial number of infections in humans 
+RH = 0 # ???
+SH = NH - EH - IH - RH
+
+SV = 20000 # ratio of mosquitoes to humans
+EV = 0 # initial number of exposed in mosquitoes (0.5*SV)
+IV = 1 # initial number of infectious in mosquitoes 
+
+times=seq(1,365) # ???
+
+state = c(SH = SH, EH = EH, IH = IH, RH = RH,
+          SV = SV, EV = EV, IV = IV)
+
+param = c(TLH = TLH, TIIT = TIIT, TEIT = TEIT, MPP = MPP, e = e,
+          IH_visit = IH_visit, TID = TID, cVH = cVH, cHV = cHV,
+          TLV = TLV)
+
+sim_basic_fog = ode(y=state,times=times,func=SEIR_basic_fog,parms=param)
+
+#matplot(sim_basic[,'time'], sim_basic[,'IV'], type = 'l', lwd = 1, col = 'blue', lty = 1)
+matplot(sim_basic_fog[,'time'], sim_basic_fog[,'IH'], type = 'l', xlim = c(0,365), lwd = 1, col = 'red',
+        lty = 1, main = 'Human Infections of Dengue', cex.main = 1, ylab = 'Prevalence of Dengue', xlab = 'Time')
+legend('topright',cex=1,seg.len = 1,
+       legend=c('IH'),
+       lty=c(1,1),lwd=c(1,1),
+       col=c('red'),bty='n')
+
+
+
+# model function for seasonality w/o insecticide use
+
+SEIR_season_no_fog <- function(t, state, parameters) {
   with(as.list(c(state, parameters)), {
     
     # real-time TLV based on season forcing (corrected) 
@@ -84,7 +157,7 @@ SEIR_vector <- function(t, state, parameters) {
     #TLV = TLV/correction.factor * (1 + b.term * Term[time])
     
     # real-time TLV based on seasonality forcing (uncorrected) 
-    TLV = TLV + (TLV.term * Term[time])
+    TLV = TLV_avg + (TLV.term * Term[times])
     
     # host population
     dSH = (NH/TLH) - (SH * (cVH * (IV/NH) + (1/TLH)))
@@ -111,11 +184,15 @@ Term=rep(1,length(times));  # initialize a vector to store the Term
 ind=(1:length(Term) %% 365) %in% wet_season  # find those days that are in wet season
 Term[ind]=-1; # set them to -1
 
+ind[1]
+
+ind[160]
+
 # parameter values for simulations
 
 
 NH = 1e4    # host population
-TLH = 68.5*365*24 # host life span is 68.5 years or 600,060 hours or 25002.5 days
+TLH = 68.5*365 # host life span is 68.5 years or 600,060 hours or 25002.5 days
 TIIT = 5  # intrinsic incubation period is 5 days
 TEIT = 10   # extrinsic incubation period is 10 days
 MPP = 2    # Number of mosquitoes per person, MPP = 2 for simulation 1-3
@@ -125,9 +202,7 @@ TID = 3   # host infection duration is 3 days, AKA gamma
 cVH = 0.75  # effective contact rate, vector to host is 0.75/day
 cHV = 0.375   # effective contact rate, host to vector is 0.375/day
 
-# average TLV
-
-TLV = 3.5
+TLV_avg = 3.5
 TLV.term = 0.5
 
 # run one of TLV for either wet or dry season
@@ -140,17 +215,16 @@ TLV.term = 0.5
 
 # initial states (not directly from paper)
 
-SH = NH - EH - IH - RH
 EH = 0  # initial number of exposed in humans (0.5*SH)
 IH =  0 # initial number of infections in humans (0.25*EH)
 RH = 0 # ???
+SH = NH - EH - IH - RH
 
-SV = MPP*NH # ratio of mosquitoes to humans
-EV = 0. # initial number of exposed in mosquitoes (0.5*SV)
-IV = 0.029*EV # initial number of infectious in mosquitoes (0.029*EV)
+SV = 20000 # ratio of mosquitoes to humans
+EV = 0 # initial number of exposed in mosquitoes (0.5*SV)
+IV = 1 # initial number of infectious in mosquitoes 
 
-times = 1:(365) # ???
-  
+
 state = c(SH = SH, EH = EH, IH = IH, RH = RH,
           SV = SV, EV = EV, IV = IV)
 
@@ -158,7 +232,7 @@ param = c(TLH = TLH, TIIT = TIIT, TEIT = TEIT, MPP = MPP, e = e,
                IH_visit = IH_visit, TID = TID, cVH = cVH, cHV = cHV,
                TLV = TLV)
 
-sim_season = ode(y=state,times=times,func=SEIR_vector,parms=param)
+sim_season = ode(y=state,times=times,func=SEIR_season_no_fog,parms=param)
 
 
 
